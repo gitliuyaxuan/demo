0c15bdc41f26e1168edb17331160a45c9b98f45b
gitliuyaxuan <491407961@qq.com>
2019-04-18 10:16:54 +0800

    01-init

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 配置路由
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 导入组件
+import Login from '@/components/login/Login'
+
+// 安装插件
+Vue.use(VueRouter)
+
+// 创建路由实例 并导出
+export default new VueRouter({
+  routes: [{ path: '/login', component: Login }]
+})
